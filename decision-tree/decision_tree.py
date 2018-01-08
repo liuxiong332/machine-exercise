1ec3409436752c99e45bac09a045d2ca5f8af89f
liuxiong <liuxiong332@163.com>
2018-01-08 09:58:27 +0800

    Create create_tree function

--- decision-tree/decision_tree.py
+++ decision-tree/decision_tree.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from math import log, inf
+import operator
 
 # 计算香农熵
 def calc_shannon_ent(dataset):
@@ -39,4 +40,25 @@ def choose_bestfeature(dataset):
             min_ent = subent
             best_axis = i
     return best_axis
-    
+
+def max_label(dataset):
+    counts = defaultdict(int)
+    for row in dataset:
+        counts[row[-1]] += 1
+    return max(counts.items(), key=operator.itemgetter(1))[0]
+
+def create_tree(dataset, labels):
+    first_val = dataset[0][-1]
+    # 所有数据都属于一个类别
+    if all(row[-1] == first_val for row in dataset):
+        return first_val
+    elif len(dataset[0]) == 1: # 没有特征属性可以进行决策
+        return max_label(dataset)   
+    axis = choose_bestfeature(dataset) #选取最合适的索引
+    feature = labels[axis]      # 根据索引获取feature名字
+    feature_tree = {}
+    val_map = split_dataset(dataset, axis)
+    for val, subset in val_map.items():
+        new_labels = labels[0:axis] + labels[axis + 1:]
+        feature_tree[val] = create_tree(subset, new_labels)
+    return { feature: feature_tree }
