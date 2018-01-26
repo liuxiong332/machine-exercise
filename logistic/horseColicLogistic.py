84336b3acf678471a566fd0acc0d65549c331be8
liuxiong <liuxiong332@163.com>
2018-01-26 10:19:49 +0800

    Add some logistic files

---
+++ logistic/horseColicLogistic.py
@@ -0,0 +1,34 @@
+import logistic
+import numpy as np
+
+def colic_test():
+  with open('horseColicTraining.txt') as fr:
+    data_mat, label_mat = [], []
+    for line in fr.readlines():
+      line_data = line.strip().split('\t')
+      line_data = [float(d) for d in line_data]
+      data_mat.append(line_data[:-1])
+      label_mat.append(line_data[-1])
+  weights = logistic.grad_ascend(data_mat, label_mat)
+  print(weights)
+
+  error_count = 0
+  with open('horseColicTest.txt') as fr:
+    lines = fr.readlines()
+    for line in lines:  
+      line_data = line.strip().split('\t')
+      line_data = [float(d) for d in line_data]
+      label_val = np.dot(np.array(line_data[:-1]), weights)
+      label = 1 if label_val >= 0 else 0
+      if label != line_data[-1]:
+        error_count += 1
+  print('the error rate is ', error_count / len(lines))
+  return error_count, len(lines)
+
+def multi_test():
+  errors, counts = 0, 0
+  for i in range(10):
+    error_count, all_count = colic_test()
+    errors += error_count
+    counts += all_count
+  print('Average error rate is ', errors / counts)
--- logistic/logistic.py
+++ logistic/logistic.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.matlib as matlib
+import math
 
 def load_dataset():
   data_mat = []
@@ -14,7 +15,7 @@ def load_dataset():
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
-def grad_ascend(data_in, label_in):
+def grad_ascend2(data_in, label_in):
   data_mat = matlib.mat(data_in)
   label_mat = matlib.mat(label_in).transpose()
 
@@ -28,3 +29,50 @@ def grad_ascend(data_in, label_in):
     weights = weights + alpha * data_mat.T * error
   return weights
 
+def grad_ascend(data_in, label_in):
+  data_mat = np.array(data_in)
+  label_mat = np.array(label_in)
+  m, n = matlib.shape(data_mat)
+  weights = np.ones(n)
+  alpha = 0.001
+  max_cycle = 150
+  for i in range(max_cycle):
+    data_index = list(range(m))    
+    for j in range(m):
+      random_index = int(np.random.uniform(0, len(data_index)))
+      alpha = 4 / (1 + i + j) + 0.01
+
+      # print(data_mat[random_index] * weights)
+      x = np.sum(data_mat[random_index] * weights)
+      if x == 0: print(x)
+      calc_labels = sigmoid(np.sum(data_mat[random_index] * weights)) # sigmoid(XW)
+      error = label_mat[random_index] - calc_labels
+      weights = weights + alpha * data_mat[random_index] * error
+      # del data_index[random_index]
+  return weights
+
+def plot_bestfit():
+  import matplotlib.pyplot as pyplot
+  data_in, label_in = load_dataset()
+  weights = grad_ascend(data_in, label_in)
+  fig = pyplot.figure()
+  ax = fig.add_subplot(111)
+  data_arr = np.array(data_in)
+  dots1x, dots1y, dots0x, dots0y = [], [], [], []
+  for i in range(data_arr.shape[0]):
+    if label_in[i] == 1:
+      dots1x.append(data_arr[i, 0])
+      dots1y.append(data_arr[i, 1])
+    else:
+      dots0x.append(data_arr[i, 0])
+      dots0y.append(data_arr[i, 1])
+  partx = np.array([-3, 3])
+  party = (-weights[2] - weights[0] * partx) / weights[1]   
+  print(partx, party)
+  ax.scatter(dots0x, dots0y, s=30, c='red')
+  ax.scatter(dots1x, dots1y, s=30, c='green')
+  ax.plot(partx, party)
+  ax.set_xlabel('X1')
+  ax.set_ylabel('X2')
+  fig.show()
+  
